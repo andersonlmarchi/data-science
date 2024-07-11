331ad1b78fc8c3b97f6b7e8c0e9eefb225f0299f
Anderson Marchi <andersonlmarchi@gmail.com>
2024-07-11 15:20:13 -0300

    fix related names

--- src/fuel_analisys/fuel_informations.py
+++ src/fuel_analisys/fuel_informations.py
@@ -1,56 +1,56 @@
-# Importar as bibliotecas necessárias
-import matplotlib.pyplot as plt
-import seaborn as sns
 import pandas as pd
-import numpy as np
+# import matplotlib.pyplot as plt
 
-# Carregar o arquivo CSV
+# get file full path
 df = pd.read_csv('files/COMBUSTIVEIS.csv')
+print(df)
 
-# Visualizar os primeiros registros do dataframe
-print(df.head())
+# variancia dos preços por cidade e tipo de combustivel
+print("Variância dos preços por cidade e tipo de combustivel")
+print(df.groupby(['CIDADE', 'COMBUSTIVEL'])['PRECO'].var())
+print("------------------------")
 
-# Calcular estatísticas descritivas para cada cidade e tipo de combustível
-statistics = df.groupby(['CIDADE', 'COMBUSTIVEL']).agg(
-    media_preco=('PRECO', 'mean'),
-    mediana_preco=('PRECO', 'median'),
-    desvio_padrao=('PRECO', 'std'),
-    variancia=('PRECO', 'var')
-).reset_index()
+# desvio padrão dos preços por cidade e tipo de combustivel
+print("Desvio padrão dos preços por cidade e tipo de combustivel")
+print(df.groupby(['CIDADE', 'COMBUSTIVEL'])['PRECO'].std())
+print("------------------------")
 
-print(statistics)
+# desvio padrão dos preços por tipo de combustível
+print("Desvio padrão dos preços por tipo de combustível")
+print(df.groupby(['COMBUSTIVEL'])['PRECO'].std())
+print("------------------------")
 
-# Configurar o estilo do seaborn
-sns.set_theme(style="whitegrid")
+# variancia dos preços por tipo de combustível
+print("Variância dos preços por tipo de combustível")
+print(df.groupby(['COMBUSTIVEL'])['PRECO'].var())
+print("------------------------")
 
-# Criar visualizações para cada tipo de combustível comparando entre as cidades
-def plot_fuel_prices_by_fuel(df):
-    unique_fuels = df['COMBUSTIVEL'].unique()
-    fig_paths = []
-    
-    for fuel in unique_fuels:
-        fuel_data = df[df['COMBUSTIVEL'] == fuel]
-        plt.figure(figsize=(12, 6))
-        palettes = sns.color_palette("husl", len(df['CIDADE'].unique()))
-        sns.boxplot(x='CIDADE', y='PRECO', hue='CIDADE', data=fuel_data, palette=palettes, legend=False)
-        plt.title(f'Preços do {fuel.capitalize()} nas Cidades')
-        plt.xlabel('Cidade')
-        plt.ylabel('Preço (R$)')
-        
-        # Definir a escala do eixo y a cada 20 centavos, específico para cada combustível
-        min_price = fuel_data['PRECO'].min()
-        max_price = fuel_data['PRECO'].max()
-        plt.yticks([round(x, 2) for x in np.arange(min_price, max_price + 0.2, 0.2)])
-        
-        # Salvar a figura
-        fig_path = f'images/precos_{fuel.lower()}_cidades.png'
-        plt.savefig(fig_path)
-        plt.close()
-        fig_paths.append(fig_path)
-    
-    return fig_paths
+# média dos preços por cidade e tipo de combustivel
+print("Média dos preços por cidade e tipo de combustivel")
+print(df.groupby(['CIDADE', 'COMBUSTIVEL'])['PRECO'].mean())
+print("------------------------")
 
-# Plotar e salvar as visualizações
-fig_paths = plot_fuel_prices_by_fuel(df)
-for path in fig_paths:
-    print(f'Figura salva em: {path}')
+# média dos preços por tipo de combustível
+print("Média dos preços por tipo de combustível")
+print(df.groupby(['COMBUSTIVEL'])['PRECO'].mean())
+print("------------------------")
+
+# mediana dos preços por cidade e tipo de combustivel
+print("Mediana dos preços por cidade e tipo de combustivel")
+print(df.groupby(['CIDADE', 'COMBUSTIVEL'])['PRECO'].median())
+print("------------------------")
+
+# mediana dos preços por tipo de combustível
+print("Mediana dos preços por tipo de combustível")
+print(df.groupby(['COMBUSTIVEL'])['PRECO'].median())
+print("------------------------")
+
+# posto com o combustivel de maior preco por cidade
+print("Posto com o combustivel de maior preco por cidade")
+print(df.loc[df.groupby(['CIDADE'])['PRECO'].idxmax()])
+print("------------------------")
+
+# posto com o combustivel de menor preco por cidade
+print("Posto com o combustivel de menor preco por cidade")
+print(df.loc[df.groupby(['CIDADE'])['PRECO'].idxmin()])
+print("------------------------")
